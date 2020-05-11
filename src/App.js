caaac9906b54d6c97bcfb3e0aac13c65b39a8551
Matthew Pombo <mppombo5@gmail.com>
2020-05-11 15:55:46 -0700

    Added navbar for different routes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,31 @@ import TodosList from "./components/todos-list.component";
 import EditTodo from "./components/edit-todo.component";
 import CreateTodo from "./components/create-todo.component";
 
+import logo from "./logo.png";
+
 class App extends Component {
     render() {
         return (
             <Router>
                 <div className="container">
-                    <h2>MERN-Stack Todo App</h2>
+
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <a className="navbar-brand" href="https://github.com/mppombo5" target="_blank">
+                            <img src={logo} width="30" height="30" alt="My GitHub homepage" />
+                        </a>
+
+                        <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item">
+                                    <Link to="/" className="nav-link">Todos</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/create" className="nav-link">Create Todo</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
                     
                     <Route path="/" exact component={TodosList} />
                     <Route path="/edit/:id" component={EditTodo} />
